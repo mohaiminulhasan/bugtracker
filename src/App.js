fa78a0c8df2cbed0fe9621825e914768c33e3a64
Mohaiminul Hasan <hasan.mohaiminul@gmail.com>
2021-04-27 07:15:30 -0400

    add routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import './App.css';
 
+import { useContext } from 'react';
+import { Route, Redirect, Switch} from 'react-router-dom';
+
 import { Login } from './pages';
 
-function App() {
+import { AuthContext } from './context/AuthContext';
+
+const AuthenticatedRoute = ({ children, ...rest }) => {
+  const authContext = useContext(AuthContext);
+
+  console.log(children);
+
   return (
-    <>
-    <Login />
-    </>
+    <Route {...rest} render={() => 
+      authContext.isAuthenticated() ? (
+        children
+      ) : (
+        <Redirect to="/" />
+      )
+    } />
   );
+}
+
+const AppRoutes = () => {
+  return (
+    <Switch>
+      <Route exact path="/" render={Login} />
+      {/* <AuthenticatedRoute path="/list">
+        <List/>
+      </AuthenticatedRoute>
+      <Route path="/invite" render={Invite} /> */}
+    </Switch>
+  );
+}
+
+function App() {
+  return <AppRoutes />;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { BrowserRouter as Router } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
