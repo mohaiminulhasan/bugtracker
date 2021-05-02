bce2b434f581695dba4508aadc354b5e2a60ef51
Mohaiminul Hasan <hasan.mohaiminul@gmail.com>
2021-05-02 18:50:17 -0400

    add user as admin

--- src/pages/RoleMgt.js
+++ src/pages/RoleMgt.js
@@ -1,11 +1,125 @@
+import { useState, useEffect } from 'react';
 import { Topbar } from "../components"
 
 export const RoleMgt = () => {
+  const [projectSlug, setProjectSlug] = useState(null);
+
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const [team, setTeam] = useState(null);
+  const [teamLoading, setTeamLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      const uri = 'http://127.0.0.1:8000/owned/projects/';
+
+      let h = new Headers();
+      h.append('Content-Type', 'application/json');
+      h.append('Authorization', 'Token ' + localStorage.getItem('token'));
+
+      let req = new Request(uri, {
+        method: 'GET',
+        headers: h,
+        mode: 'cors'
+      });
+
+      const response = await fetch(req);
+      const data = await response.json();
+      
+      setData(data);
+      setLoading(false);
+    }
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    async function fetchData() {
+      const uri = `http://127.0.0.1:8000/teamusers/${projectSlug}/`;
+
+      let h = new Headers();
+      h.append('Content-Type', 'application/json');
+      h.append('Authorization', 'Token ' + localStorage.getItem('token'));
+
+      let req = new Request(uri, {
+        method: 'GET',
+        headers: h,
+        mode: 'cors'
+      });
+
+      const response = await fetch(req);
+      const data = await response.json();
+
+      setTeam(data);
+      setTeamLoading(false);
+    }
+
+    projectSlug && fetchData();
+  }, [projectSlug]);
+
+  const toggleAdmin = async e => {
+    let username = e.target.value;
+
+    const uri = `http://127.0.0.1:8000/toggle/${username}/to/${projectSlug}/as/admin/`;
+
+    let h = new Headers();
+    h.append('Content-Type', 'application/json');
+    h.append('Authorization', 'Token ' + localStorage.getItem('token'));
+
+    let req = new Request(uri, {
+      method: 'POST',
+      headers: h,
+      mode: 'cors'
+    });
+
+    const response = await fetch(req);
+    const data = await response.json();
+
+    let index = team.findIndex(x => x.username === username);
+    let obj = team.find(x => x.username === username);
+
+    let temp;
+    if (data.response === 'OK') {
+      obj.isAdmin = !obj.isAdmin;
+
+      temp = [...team];
+      temp[index] = obj;
+    }
+
+    setTeam(temp);
+  }
+
   return (
     <>
     <Topbar />
+
     <h3>Role Management</h3>
 
+    <div style={{ display: 'flex' }}>
+      <div>
+        <select name="projects" id="projects" size="5" style={{width: 200}}>
+          {loading ? <option>...loading</option> : 
+            data.map((item, index) => <option onClick={() => setProjectSlug(item.slug)} key={index} value={item.slug}>{item.title}</option>)
+          }
+        </select>
+      </div>
+
+      <div>
+        <span>Users in this project</span> <br/>
+
+        { teamLoading ? <div>Please select a project.</div> :
+          team.map((item, index) => {
+            return <div key={index}>
+              <label>
+                <input type='checkbox' name={item.username} value={item.username} checked={item.isAdmin} onChange={toggleAdmin} />
+                {item.username}
+              </label>
+            </div>
+          })
+        }
+      </div>
+    </div>
     </>
   );
 }
