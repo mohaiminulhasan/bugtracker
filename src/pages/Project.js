d15c969ffad612d6c5686cb1b4e6c05931c36002
Mohaiminul Hasan <hasan.mohaiminul@gmail.com>
2021-05-07 14:48:48 -0400

    fix drag and drop

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -46,6 +46,20 @@ export const Project = () => {
     fetchData();
   }, [projectSlug]);
 
+  const moveElementInState = (ticket_id, prev_board_id, next_board_id) => {
+    let tempObj = {};
+    let temp = {...data};
+    let obj = temp[prev_board_id].find(x => x.id === parseInt(ticket_id));
+
+    if (typeof obj !== "undefined") {
+      tempObj = obj;
+      temp[prev_board_id] = temp[prev_board_id].filter(x => x.id !== parseInt(ticket_id));
+      temp[next_board_id].push(tempObj);
+    }
+
+    setData(temp);
+  }
+
   const handleClick = (ticket_id) => {
     history.push(`${url}/ticket/${ticket_id}`)
   }
@@ -66,6 +80,7 @@ export const Project = () => {
                   projectSlug={projectSlug}
                   heading={statusDict[status]}
                   addTicket={addTicket}
+                  moveElementInState={moveElementInState } 
                   className='border border-gray-500 rounded-t-md board'>
           {
             data[status].map((item, index) => <Ticket 
