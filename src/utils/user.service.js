ed7ed2cc36a131ea6d6f70ade815942a25ecacd7
Mohaiminul Hasan <hasan.mohaiminul@gmail.com>
2021-05-23 07:49:30 -0400

    conditional domain for API calls 2

--- src/utils/user.service.js
+++ src/utils/user.service.js
@@ -3,7 +3,7 @@ export const userService = {
 }
 
 async function login(username, password) {
-    const uri = 'http://127.0.0.1:8000/api-token-auth/';
+    const uri = window.location.hostname === 'localhost' ? 'http://127.0.0.1:8000/api-token-auth/' : 'https://minigunnr.pythonanywhere.com/api-token-auth/';
 
     let h = new Headers();
     h.append('Content-Type', 'application/json')
