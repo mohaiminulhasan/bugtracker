b823ad779f7a62badd777d191cc59c46696b0d59
Mohaiminul Hasan <hasan.mohaiminul@gmail.com>
2021-05-23 07:47:00 -0400

    conditional domain for API calls

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -5,7 +5,7 @@ const { Provider } = AppContext;
 
 const AppProvider = ({ children }) => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
-  const apiUrl = 'http://127.0.0.1:8000';
+  const apiUrl = window.location.hostname === 'localhost' ? 'http://127.0.0.1:8000' : 'https://minigunnr.pythonanywhere.com';
   
   return (
       <Provider value={{
